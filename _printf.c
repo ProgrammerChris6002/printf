5586c459a74e85a29bbc29e759c70103ce3ff777
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-30 21:07:51 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,48 +1,55 @@
 #include "main.h"
 
+int print_char(const char *c) {
+    write(1, c, 1);
+    return 1;
+}
+
+int print_str(const char *str) {
+    int str_len = 0;
+    while (str[str_len] != '\0') {
+        str_len++;
+    }
+    write(1, str, str_len);
+    return str_len;
+}
+
 int _printf(const char *format, ...)
 {
-	int chara_print = 0;
-	va_list args;
+    int chara_print = 0;
+    va_list args;
 
-	if (format == NULL)
-		return (-1);
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			chara_print++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				chara_print++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				write(1, &c, 1);
-				chara_print++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char*);
-				int str_len = 0;
-				while (str[str_len] != '\0')
-					str_len++;
-				write(1, str, str_len);
-				chara_print += str_len;
-			}
-		}
-		format++;
-	}
-	va_end(args);
-	return (chara_print);
+    if (format == NULL)
+        return (-1);
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            chara_print += print_char(format);
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            if (*format == '%')
+            {
+                chara_print += print_char(format);
+            }
+            else if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                chara_print += print_char(&c);
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char*);
+                chara_print += print_str(str);
+            }
+        }
+        format++;
+    }
+    va_end(args);
+    return chara_print;
 }
--- main.h
+++ main.h
@@ -6,6 +6,8 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int print_char(const char *c);
+int print_str(const char *str);
 int binary(int num);
 int octal(int num);
 
