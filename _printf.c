91dbd7e3f88d22a8fde56592778e04e580ccc69d
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-30 21:11:22 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,55 +1,82 @@
 #include "main.h"
 
-int print_char(const char *c) {
-    write(1, c, 1);
-    return 1;
+/**
+ * print_char - Prints a character to the standard output
+ * @c: The character to be printed
+ *
+ * Return: The number of characters printed (always 1)
+ */
+
+int print_char(const char c)
+{
+	write(1, &c, 1);
+	return (1);
 }
 
-int print_str(const char *str) {
-    int str_len = 0;
-    while (str[str_len] != '\0') {
-        str_len++;
-    }
-    write(1, str, str_len);
-    return str_len;
+/**
+ * print_str - Prints a string to the standard output
+ * @str: The string to be printed
+ *
+ * Return: The number of characters printed
+ */
+
+int print_str(const char *str)
+{
+	int str_len = 0;
+
+	while (str[str_len] != '\0')
+		str_len++;
+
+	write(1, str, str_len);
+	return (str_len);
 }
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ *
+ * Return: The number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
-    int chara_print = 0;
-    va_list args;
+	int chara_print = 0;
+	va_list args;
 
-    if (format == NULL)
-        return (-1);
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            chara_print += print_char(format);
-        }
-        else
-        {
-            format++;
-            if (*format == '\0')
-                break;
-            if (*format == '%')
-            {
-                chara_print += print_char(format);
-            }
-            else if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                chara_print += print_char(&c);
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char*);
-                chara_print += print_str(str);
-            }
-        }
-        format++;
-    }
-    va_end(args);
-    return chara_print;
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			chara_print += print_char(*format);
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				chara_print += print_char(*format);
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				chara_print += print_char(c);
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				chara_print += print_str(str);
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (chara_print);
 }
